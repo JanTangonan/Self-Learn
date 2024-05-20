6077a0cae1230448698649ee699e3d12b180bd61
antiwallbreaker <antiwallbreaker@gmail.com>
2024-05-20 06:38:13 +0000

    Added new files

---
+++ self-learn/src/components/LinkedList.js
@@ -0,0 +1,32 @@
+export default function LinkedList(){
+    // Node class
+    class Node {
+        constructor(data) {
+        this.data = data;
+        this.next = null;
+        }
+    }
+  
+  // Linked list class
+  class LinkedList {
+    constructor() {
+      this.head = null;
+    }
+  
+    // Add a node at the beginning
+    add(data) {
+      let newNode = new Node(data);
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+  
+    // Traverse the linked list
+    traverse() {
+      let current = this.head;
+      while (current !== null) {
+        console.log(current.data);
+        current = current.next;
+      }
+    }
+  }
+}
--- self-learn/src/index.js
+++ self-learn/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import Calculator from './components/Calculator';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Calculator />
   </React.StrictMode>
 );
 
