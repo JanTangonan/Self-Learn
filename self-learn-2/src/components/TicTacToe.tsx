f9ffd6c498c1e4f509383da99fbb1cead1bc71bb
antiwallbreaker <antiwallbreaker@gmail.com>
2024-06-02 13:52:20 +0000

    additions

--- self-learn-2/src/components/TicTacToe.tsx
+++ self-learn-2/src/components/TicTacToe.tsx
@@ -1,123 +1,56 @@
-// import '../styles/tictactoe.css';
-// import { Button, ButtonGroup, Center, Heading } from '@chakra-ui/react'
+import React, { useState } from 'react';
 
-// import { useState } from 'react';
+type Player = 'X' | 'O' | null;
 
-// function Square({ value, onSquareClick }) {
-//   return (
-//     <button className="square" onClick={onSquareClick}>
-//       {value}
-//     </button>
-//   );
-// }
+const TicTacToe: React.FC = () => {
+  const [board, setBoard] = useState<Player[]>(Array(9).fill(null));
+  const [currentPlayer, setCurrentPlayer] = useState<Player>('X');
 
-// function Board({ xIsNext, squares, onPlay }) {
-//   function handleClick(i) {
-//     if (calculateWinner(squares) || squares[i]) {
-//       return;
-//     }
-//     const nextSquares = squares.slice();
-//     if (xIsNext) {
-//       nextSquares[i] = 'X';
-//     } else {
-//       nextSquares[i] = 'O';
-//     }
-//     onPlay(nextSquares);
-//   }
+  const handleCellClick = (index: number) => {
+    if (board[index] || calculateWinner(board)) {
+      return;
+    }
 
-//   const winner = calculateWinner(squares);
-//   let status;
-//   if (winner) {
-//     status = 'Winner: ' + winner;
-//   } else {
-//     status = 'Next player: ' + (xIsNext ? 'X' : 'O');
-//   }
+    const updatedBoard = [...board];
+    updatedBoard[index] = currentPlayer;
+    setBoard(updatedBoard);
 
-//   return (
-//     <>
-//       <div className="status">
-//         <Center><Heading>{status}</Heading></Center>
-//       </div>
-//       <div className="board-row">
-//         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-//         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-//         <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-//       </div>
-//       <div className="board-row">
-//         <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-//         <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-//         <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-//       </div>
-//       <div className="board-row">
-//         <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-//         <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-//         <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-//       </div>
-//     </>
-//   );
-// }
+    setCurrentPlayer(currentPlayer === 'X' ? 'O' : 'X');
+  };
 
-// export default function Game() {
-//   const [history, setHistory] = useState([Array(9).fill(null)]);
-//   const [currentMove, setCurrentMove] = useState(0);
-//   const xIsNext = currentMove % 2 === 0;
-//   const currentSquares = history[currentMove];
+  const renderCell = (index: number) => (
+    <div className="cell" onClick={() => handleCellClick(index)}>
+      {board[index]}
+    </div>
+  );
 
-//   function handlePlay(nextSquares) {
-//     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
-//     setHistory(nextHistory);
-//     setCurrentMove(nextHistory.length - 1);
-//   }
+  const winner = calculateWinner(board);
 
-//   function jumpTo(nextMove) {
-//     setCurrentMove(nextMove);
-//   }
+  return (
+    <div className="tic-tac-toe">
+      <div className="board">
+        {board.map((_, index) => renderCell(index))}
+      </div>
+      {winner && <div className="winner">Winner: {winner}</div>}
+    </div>
+  );
+};
 
-//   const moves = history.map((squares, move) => {
-//     let description;
-//     if (move > 0) {
-//       description = 'Go to move #' + move;
-//     } else {
-//       description = 'Go to game start';
-//     }
-//     return (
-//       <li key={move}>
-//         <Button className="moves" onClick={() => jumpTo(move)}>{description}</Button>
-//       </li>
-//     );
-//   });
+const calculateWinner = (board: Player[]): Player | null => {
+  const winningPositions = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+    [0, 4, 8], [2, 4, 6] // Diagonals
+  ];
 
-//   return (
-//     <div className="game-container">
-//       <div className="game">
-//         <div className="game-board">
-//           <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
-//         </div>
-//         <div className="game-info">
-//           <ol>{moves}</ol>
-//         </div>
-//       </div>
-      
-//     </div>
-//   );
-// }
+  for (const positions of winningPositions) {
+    const [a, b, c] = positions;
+    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+      return board[a];
+    }
+  }
 
-// function calculateWinner(squares) {
-//   const lines = [
-//     [0, 1, 2],
-//     [3, 4, 5],
-//     [6, 7, 8],
-//     [0, 3, 6],
-//     [1, 4, 7],
-//     [2, 5, 8],
-//     [0, 4, 8],
-//     [2, 4, 6],
-//   ];
-//   for (let i = 0; i < lines.length; i++) {
-//     const [a, b, c] = lines[i];
-//     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-//       return squares[a];
-//     }
-//   }
-//   return null;
-// }
+  return null; // No winner
+};
+
+export default TicTacToe;
