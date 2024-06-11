95c03795ba1cb4496a10ca6bb2da11d0b7e03bc3
JanTangonan <janthegreatxc@gmail.com>
2024-06-11 11:24:16 +0800

    added bubble sort and selection sort

--- self-learn-2/src/components/App.tsx
+++ self-learn-2/src/components/App.tsx
@@ -6,17 +6,21 @@ import Calculator from './Calculator';
 import Statistics from './Statistics';
 import Forms from './Forms';
 import DashBoard from './DashBoard';
+import BubbleSort from './BubbleSort';
+import SelectionSort from './SelectionSort';
 
 function App() {
   return (
     <div className='App'>
-      <NavBar />
+      {/* <NavBar /> */}
       <Routes>
         <Route path="home" element={<LandingPage />} />
         <Route path="tictactoe" element={<TicTacToe />} />
         <Route path="calculator" element={<Calculator />} />
         <Route path="forms" element={<Forms />} />
         <Route path="dashboard" element={<DashBoard />} />
+        <Route path="bubblesort" element={<BubbleSort />} />
+        <Route path="selectionsort" element={<SelectionSort />} />
       </Routes>
     </div>
   );
