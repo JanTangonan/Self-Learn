7aac553edb79935307e6d78020fb73c5b3bae173
Jumba <janthegreatxc@gmail.com>
2024-06-16 22:00:36 +0800

    simple additions

--- self-learn-2/src/components/DashBoard.tsx
+++ self-learn-2/src/components/DashBoard.tsx
@@ -1,18 +1,25 @@
 import { Grid, GridItem } from "@chakra-ui/react";
+import '../styles/DashBoard.css'
 
 
 export default function DashBoard() {
     return (
-        <Grid
-            h="1200px"
-            templateRows="repeat(12, 1fr)"
-            templateColumns="repeat(5, 1fr)"
-            gap={4}
-        >
-            <GridItem rowSpan={12} colSpan={1} bg="tomato" />
-            <GridItem colSpan={2} bg="papayawhip" />
-            <GridItem colSpan={2} bg="papayawhip" />
-            <GridItem colSpan={4} bg="tomato" />
-        </Grid>
+        <>  
+            <div className="margin"></div>
+            <Grid
+                h="1200px"
+                templateRows="repeat(12, 1fr)"
+                templateColumns="repeat(5, 1fr)"
+                gap={4}
+            >
+                <GridItem rowSpan={12} colSpan={1} bg="tomato" />
+
+                <GridItem rowSpan={3} colSpan={2} bg="papayawhip" borderRadius={20}/>
+                <GridItem rowSpan={3} colSpan={2} bg="papayawhip" borderRadius={20}/>
+                <GridItem rowSpan={3} colSpan={2} bg="tomato" borderRadius={20}/>
+                <GridItem rowSpan={3} colSpan={2} bg="tomato" borderRadius={20}/>
+            </Grid>
+        </>
+        
     );
     }
